7e0c54a82a454e62e1ab4dbfa10292a431b84af8
androsgithub <andreemaprinc@gmail.com>
2024-11-04 02:42:07 -0300

    feat: add a history every time when the functions are used

--- NewTalents.App/Calculadora.cs
+++ NewTalents.App/Calculadora.cs
@@ -7,12 +7,44 @@ namespace NewTalents.App
 {
     public class Calculadora
     {
-        public int Somar(int valor1, int valor2) { return valor1 + valor2; }
-        public int Subtrair(int valor1, int valor2) { return valor1 - valor2; }
-        public int Multiplicar(int valor1, int valor2) { return valor1 * valor2; }
-        public int Dividir(int valor1, int valor2) { return valor1 / valor2; }
+        private List<string> _historico { get; set; }
+        public Calculadora()
+        {
+            _historico = new List<string>();
+        }
+        public int Somar(int valor1, int valor2)
+        {
+            var result = valor1 + valor2;
+            _historico.Insert(0, $"Soma: {valor1} + {valor2} = {result}");
+            return result;
+        }
+        public int Subtrair(int valor1, int valor2)
+        {
+            var result = valor1 - valor2;
+            _historico.Insert(0, $"Subtrair: {valor1} - {valor2} = {result}");
+            return result;
+        }
+        public int Multiplicar(int valor1, int valor2)
+        {
+            var result = valor1 * valor2;
+            _historico.Insert(0, $"Multiplicar: {valor1} * {valor2} = {result}");
+            return result;
+        }
+        public int Dividir(int valor1, int valor2)
+        {
+            var result = valor1 / valor2;
+            _historico.Insert(0, $"Multiplicar: {valor1} / {valor2} = {result}");
+            return result;
+        }
 
-        public List<string> Historico() { return new List<string>(); }
+        public List<string> Historico()
+        {
+            if (_historico.Count > 3)
+            {
+                _historico.RemoveRange(3, _historico.Count);
+            }
+            return _historico;
+        }
 
     }
 }
